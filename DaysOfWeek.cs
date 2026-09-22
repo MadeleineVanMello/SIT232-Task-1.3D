60550a84496c2f77384b9dde725671ce0c4bbd43
Madeleine Van Mello <s226440666@deakin.edu.au>
2026-09-22 11:24:07 +1000

    Add DaysOfWeek custom iterator implementation

---
+++ DaysOfWeek.cs
@@ -0,0 +1,38 @@
+﻿// using System.Collections;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         var days = new DaysOfWeek();
+
+//         Console.WriteLine("Iterating through DaysOfWeek:");
+//         foreach (var day in days)
+//         {
+//             Console.WriteLine(day);
+//             Console.WriteLine("\nPress any key to continue...");
+//             Console.ReadKey();
+//         }
+
+//         Console.WriteLine("\nPress any key to exit...");
+//         Console.ReadKey();
+//     }
+// }
+
+// // Custom collection with iterator
+// public class DaysOfWeek : IEnumerable<string>
+// {
+//     private string[] days =
+//         { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+//     public IEnumerator<string> GetEnumerator()
+//     {
+//         foreach (var day in days)
+//         {
+//             Console.WriteLine($"\nyielding {day}");
+//             yield return day;
+//         }
+//     }
+
+//     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+// }

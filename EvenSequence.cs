990e8913bceebb7dee8759e9d534e9cc25621404
Madeleine Van Mello <s226440666@deakin.edu.au>
2026-09-22 11:24:59 +1000

    Add EvenSequence iterator using yield return implementation

---
+++ EvenSequence.cs
@@ -0,0 +1,29 @@
+﻿// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Even numbers between 1 and 20:");
+//         foreach (var n in EvenSequence(1, 20))
+//         {
+//             Console.WriteLine(n);
+//             Console.WriteLine("\nPress any key to continue...");
+//             Console.ReadKey();
+//         }
+
+//         Console.WriteLine("\nPress any key to exit...");
+//         Console.ReadKey();
+//     }
+
+//     // Iterator method
+//     public static IEnumerable<int> EvenSequence(int first, int last)
+//     {
+//         for (int n = first; n <= last; n++)
+//         {
+//             if (n % 2 == 0)
+//             {
+//                 Console.WriteLine($"\nyielding {n}");
+//                 yield return n;
+//             }
+//         }
+//     }
+// }
